a8a8be41bc0268d468752bbdc1c402c6348f9810
Emanuele <ebellocchia@gmail.com>
2020-04-11 11:35:18 +0200

    Add new modules import.

--- py_crypto_hd_wallet/__init__.py
+++ py_crypto_hd_wallet/__init__.py
@@ -1,4 +1,6 @@
 # Version
 from ._version  import __version__
 # Libraries
-from .hd_wallet import HdWallet, HdWalletChanges, HdWalletCoins, HdWalletSpecs, HdWalletWordsNum
+from .hd_wallet_enum    import *
+from .hd_wallet_factory import HdWalletFactory
+from .hd_wallet         import HdWallet
