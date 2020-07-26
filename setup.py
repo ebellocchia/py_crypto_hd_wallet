0417f7f4cb6a546d6f130cb52bd1da39a4e9bd42
Emanuele <ebellocchia@gmail.com>
2020-07-26 17:59:02 +0200

    Add new coins to setup.py

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setuptools.setup(
     download_url="https://github.com/ebellocchia/py_crypto_hd_wallet/archive/v%s.tar.gz" % version,
     license="MIT",
     test_suite="tests",
-    install_requires = ["bip_utils~=1.0.0"],
+    install_requires = ["bip_utils~=1.1.0"],
     packages=["py_crypto_hd_wallet"],
-    keywords="bitcoin, litecoin, dogecoin, dash, ethereum, ripple, wallet, hd-wallet, bip39, bip32, bip44, bip49, bip84, python",
+    keywords="bitcoin, bitcoin cash, bitcoinsv, litecoin, dogecoin, dash, ethereum, ripple, wallet, hd-wallet, bip39, bip32, bip44, bip49, bip84, python",
     platforms = ["any"],
     classifiers=[
         "Programming Language :: Python :: 3",
